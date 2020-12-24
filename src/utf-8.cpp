3d782940c396cc631e2b6ed485a0e85bcff8f9b9
Matthew "strager" Glazar <strager.nds@gmail.com>
2020-12-23 22:26:19 -0800

    STATE MACHIN

--- src/utf-8.cpp
+++ src/utf-8.cpp
@@ -16,6 +16,7 @@
 
 #include <quick-lint-js/narrow-cast.h>
 #include <quick-lint-js/padded-string.h>
+#include <quick-lint-js/unreachable.h>
 #include <quick-lint-js/utf-8.h>
 #include <quick-lint-js/warning.h>
 
@@ -54,14 +55,136 @@ QLJS_WARNING_IGNORE_GCC("-Wattributes")
   auto is_continuation_byte = [](std::uint8_t byte) noexcept -> bool {
     return (byte & 0b1100'0000) == 0b1000'0000;
   };
+
+  enum byte_class : signed char {
+    X = 0,
+    A, // 0x00..0x7f
+    B, // 0x80..0x8f
+    C, // 0xc2..0xdf
+    D, // 0xe0
+    E, // 0xa0..0xbf
+    F, // 0xe1..0xec
+  };
+  static constexpr byte_class byte_to_class[256/*@@@*/] = {
+    A,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A, // 0x00..0x0f
+    A,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A, // 0x10..0x1f
+    A,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A, // 0x20..0x2f
+    A,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A, // 0x30..0x3f
+    A,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A, // 0x40..0x4f
+    A,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A, // 0x50..0x5f
+    A,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A, // 0x60..0x6f
+    A,A,A,A,A,A,A,A,A,A,A,A,A,A,A,A, // 0x70..0x7f
+    B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B, // 0x80..0x8f
+    X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X, // 0x90..0x9f
+    E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E, // 0xa0..0xaf
+    E,E,E,E,E,E,E,E,E,E,E,E,E,E,E,E, // 0xb0..0xbf
+    X,X,C,C,C,C,C,C,C,C,C,C,C,C,C,C, // 0xc0..0xcf
+    C,C,C,C,C,C,C,C,C,C,C,C,C,C,C,C, // 0xd0..0xdf
+    D,F,F,F,F,F,F,F,F,F,F,F,F, // 0xe0..0xef
+  };
+
+  enum class state : signed char {
+    nyi = -50, // @@@ delete
+    er2 = -12,
+    er1 = -11,
+    ok3 = -3,
+    ok2 = -2,
+    ok1 = -1,
+    initial = 0,
+
+    // U+0080..U+07FF
+    c, // 0xc2..0xdf
+
+    // U+0800..U+0FFF
+    d, // 0xe0
+    de, // 0xe0 0xa0..0xbf
+
+    // U+1000..U+CFFF
+    f, // 0xe1..0xec
+    ff, // 0xe1..0xec 0x80..0xbf
+  };
+
+  static constexpr state state_transitions[][6] = {
+    // initial     c           d           de          f           ff
+      {state::nyi, state::er1, state::er1, state::er2, state::nyi, state::nyi}, // X @@@ delete
+      {state::ok1, state::er1, state::er1, state::er2, state::er1, state::er2}, // A 0x00..0x7f
+      {state::er1, state::ok2, state::er1, state::ok3, state::ff,  state::ok3}, // B 0x80..0x8f
+      {state::c,   state::er1, state::er1, state::er2, state::er1, state::er2}, // C 0xc2..0xdf
+      {state::d,   state::er1, state::er1, state::er2, state::er1, state::er2}, // D 0xe0
+      {state::er1, state::ok2, state::de,  state::ok3, state::ff,  state::ok3}, // E 0xa0..0xbf
+      {state::f,   state::er1, state::er1, state::er2, state::er1, state::er2}, // F 0xe1..0xec
+  };
+  static auto is_end_state = [](state s) -> bool {
+    return static_cast<signed char>(s) < 0;
+  };
+
   const std::uint8_t* c = reinterpret_cast<const std::uint8_t*>(input.data());
+  // @@@ fold this into the state machine.
   if (input.size() == 0) {
     return decode_utf_8_result{
         .size = 0,
         .code_point = 0,
         .ok = false,
     };
-  } else if (c[0] <= 0x7f) {
+  }
+
+  const std::uint8_t* cc = c; // @@@ rename
+  state current_state = state::initial;
+  for (;;) {
+    current_state = state_transitions[byte_to_class[*cc]][static_cast<signed char>(current_state)];
+    if (is_end_state(current_state)) {
+      break;
+    }
+    ++cc;
+  }
+  switch (current_state) {
+    case state::initial:
+    case state::c:
+    case state::f:
+    case state::ff:
+    case state::d:
+    case state::de:
+      QLJS_UNREACHABLE();
+      break;
+    case state::er1:
+      return decode_utf_8_result{
+          .size = 1,
+          .code_point = 0,
+          .ok = false,
+      };
+    case state::er2:
+      return decode_utf_8_result{
+          .size = 2,
+          .code_point = 0,
+          .ok = false,
+      };
+    case state::ok1:
+      return decode_utf_8_result{
+          .size = 1,
+          .code_point = *c,
+          .ok = true,
+      };
+    case state::ok2:
+      return decode_utf_8_result{
+          .size = 2,
+          .code_point =
+              (char32_t(c[0] & 0b0001'1111) << 6) | (c[1] & 0b0011'1111),
+          .ok = true,
+      };
+    case state::ok3:
+      return decode_utf_8_result{
+          .size = 3,
+          .code_point = (char32_t(c[0] & 0b0000'1111) << 12) |
+                        (char32_t(c[1] & 0b0011'1111) << 6) |
+                        (c[2] & 0b0011'1111),
+          .ok = true,
+      };
+    case state::nyi:
+      goto nyi;
+  }
+
+nyi:
+  if (c[0] <= 0x7f) {
     // 1-byte sequence (0x00..0x7f, i.e. ASCII).
     return decode_utf_8_result{
         .size = 1,
